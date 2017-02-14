625b9eca760e735019d307f2a2b160589901c847
Kanika Sood <kanika901@gmail.com>
2017-02-13 20:51:41 -0800

    added code to detect multiple faces and save them as separate images.

--- happiness_detection.m
+++ happiness_detection.m
@@ -11,9 +11,11 @@ images = ('/Users/kanikas/Documents/MyProjects/HappinessDetection/images');
 zones=containers.Map;
 imageList = dir(images);
 numImages = length(imageList);
-image1 = '/Users/kanikas/Documents/MyProjects/HappinessDetection/images/LastZone.png';
-getImage(images, '/Users/kanikas/Documents/MyProjects/HappinessDetection/images/LastZone.png', numImages, imageList);
-detectFaces(image1)
+image1 = '/Users/kanikas/Documents/MyProjects/HappinessDetection/images/Lastzone.png';
+getImage(images, '/Users/kanikas/Documents/MyProjects/HappinessDetection/images/Lastzone.png', numImages, imageList);
+detectFaces(image1);
+
+
 function trainImages = getImage(images, image1, numImages, imageList)
 imageLocation = strcat(images, '/');
 for i = 3:numImages
@@ -25,22 +27,38 @@ function detectFaces(image1)
 faceDetector = vision.CascadeObjectDetector; %detects faces with Viola-Jones algorithm
 image = imread(image1);
 bboxes = step(faceDetector, image);
+
 [noOfFaces, y] = size(bboxes);
 countFacesPerZone(noOfFaces);
 faces = insertObjectAnnotation(image, 'rectangle', bboxes, 'Face');
 figure, imshow(faces), title('Detected faces')
+formSubImages(bboxes, image1, noOfFaces);
 end
 
 function numberOfFaces =countFacesPerZone(noOfFaces)
 numberOfFaces = noOfFaces;
-disp(numberOfFaces);
+fprintf('Number of faces in the images are: %i', numberOfFaces);
+%disp(numberOfFaces);
 end
 
-function detectHappyFace(image1)
+function formSubImages(bboxes, image1, noOfFaces)
+img = imread(image1);
+bbox = bbox2points(bboxes);
+for i = 1:length(bboxes)
+    subImage = imcrop(img, bboxes(i,:));
+    face = insertObjectAnnotation(subImage, 'rectangle', bboxes(i,:), 'Face');% imcrop();
+    figure, imshow(face), title('Detected faces');
+    imshow(subImage);
+    subImageName = strcat('/Users/kanikas/Documents/MyProjects/HappinessDetection/zones/zone2/Zone2_', num2str(i),'.png');
+    imwrite(subImage,subImageName);
+end
+end
+
+function detectHappyFace()
 
 end
 
-function[happiness] = computeHappinessFactor(happyFaces, totalFaces)
+function happiness = computeHappinessFactor(happyFaces, totalFaces)
 happiness = happyFaces/totalFaces;
 end
 
